dd4dbb1b392cd2f705c3ac49412448eb52ce0e13
Alex Herrera <pivothdv@gmail.com>
2021-12-10 02:02:48 -0600

    Removed ticker function & moved updateClockTick to main

--- js/script.main.js
+++ js/script.main.js
@@ -4,23 +4,16 @@ $(document).ready(function() {
 
   // Once this script is loaded, call the other clock
   // script that the DOM can be manipulated
-  //pageIsReady();
+  pageIsReady();
 
   // Create a repeatable ticker object which calls upon
   // other objects
-  initTicker();
+  var interval = setInterval(function() {
+    // Call tick function
+    updateClockTick();
+    
+  }, 1000);
 
 });
 
 
-// The ticker that calls other functions
-// that may want to update/refresh every second
-function initTicker() {
-
-  var interval = setInterval(function() {
-     // Call tick function
-     onTick();
-  }, 1000);
-
-
-}
